294d2fb260b9ca5269ff84ba1123a9c1eff4a800
Christopher <christopher.carnivale@gmail.com>
2019-01-26 14:29:55 -0500

    Changed HTML format

--- index.js
+++ index.js
@@ -2,36 +2,37 @@
 /*global $*/
 
 const api_key = 'IIwKvgVyokkngr9dQP4DXTYHTiIWMx2yJ5PHF2L';
-const BASE_URL = 'https://api.nps.gov/api/v1/parks/';
+const BASE_URL = 'https://api.nps.gov/api/v1/parks';
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  console.log(queryItems); 
   return queryItems.join('&');
 }
 
 function displayResults(responseJson) {
+  console.log(responseJson); 
   $('.results-list').empty();
   for (let i = 0; i < responseJson.data.length; i++){
     $('.results-list').append(
       `<div class="results-wrapper">
-      <h3 for="park-name">${responseJson.data[i].name}</h3>
-      <h4 for="park-address">${responseJson.data[i].latLong}</h4>
+      <h3 for="park name and link"><a href='${responseJson.data[i].url}'>${responseJson.data[i].name}</a></h3>
       <p for="park description">${responseJson.data[i].description}</p>
-      <a href='${responseJson.data[i].url}' for="link to park page">Link</a>
       </div>`
     );}
 }
 
 function getParks(query, limit=10) {
   const params = {
-    key: api_key,
+    api_key: api_key,
     stateCode: query,
     limit,
   };
   
   const queryString = formatQueryParams(params);
   const url = BASE_URL + '?' + queryString;
+  console.log(url); 
 
   fetch(url)
     .then(response => {
@@ -49,8 +50,8 @@ function getParks(query, limit=10) {
 function watchForm() {
   $('.js-parks-search-form').on( 'submit', function(event) {
     event.preventDefault();
-    const searchTerm = $('.js-state-select').val();
-    const limit = ($('.js-number-input').val() - 1 );
+    const searchTerm = $('.js-state-select').val(); 
+    const limit = ($('.js-number-input').val() - 1);
     getParks( searchTerm, limit );
   });
 }
